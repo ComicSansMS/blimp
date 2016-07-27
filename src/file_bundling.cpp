c41e1f30acde1d1a2cad95a20b3f84d52a373af6
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2016-07-27 08:17:34 +0200

    documentation for file bundling

--- src/file_bundling.cpp
+++ src/file_bundling.cpp
@@ -9,12 +9,16 @@ std::vector<FileBundleInfo> bundleFiles(std::vector<FileInfo> const& files, std:
 {
     std::vector<FileBundleInfo> ret;
     ret.reserve(files.size());
-    // if the total accumulated data is greater min_unbundled_size, no bundle will be smaller than min_unbundled_size
+    // the algorithm traverses the file list, adding small elements to the current bundle;
+    // once the current bundle size exceeds 3*min_unbundled_size, it gets cut into two bundles which both
+    // exceed min_unbundled_size; elements with higher indices move to the bundle with the new id, which then
+    // becomes the new current bundle.
+    // if the total accumulated data is greater min_unbundled_size, no bundle will be smaller than min_unbundled_size.
     std::vector<std::size_t> bundle_elements;
-    std::uint32_t bundle_id = 1u;
-    std::uintmax_t size_acc = 0;
-    std::uintmax_t cut_point = 0;
-    std::size_t cut_element_index = 0;
+    std::uint32_t bundle_id = 1u;       // running id of the current bundle assigned during list traversal
+    std::uintmax_t size_acc = 0;        // accumulated size of all elements in current bundle
+    std::uintmax_t cut_point = 0;       // byte offset into the current bundle at which we could split off a bundle
+    std::size_t cut_element_index = 0;  // index of the element preceding cut_point
     for(int i = 0; i < files.size(); ++i) {
         auto const& f = files[i];
         if(f.size < min_unbundled_size) {
@@ -24,25 +28,28 @@ std::vector<FileBundleInfo> bundleFiles(std::vector<FileInfo> const& files, std:
             bundle_elements.push_back(i);
             size_acc += f.size;
             if((cut_point == 0) && (size_acc > min_unbundled_size)) {
+                // place initial cut point;
+                // this happens only once in the beginning when we first exceed min_unbundled_size
                 cut_point = size_acc;
                 cut_element_index = i;
             }
             if(size_acc > 3*min_unbundled_size) {
+                // bundle is big enough to allow cutting into two, both bigger than min_unbundled_size
                 GHULBUS_ASSERT(cut_point != 0);
-                // bundle everything up to cut_point
+                // move cut_point
                 size_acc -= cut_point;
                 GHULBUS_ASSERT(size_acc > min_unbundled_size);
                 cut_point = size_acc;
-                ++bundle_id;
                 // elements below cut point are committed to their bundle
                 auto const cut_it = std::upper_bound(begin(bundle_elements), end(bundle_elements), cut_element_index);
                 GHULBUS_ASSERT(cut_it != end(bundle_elements));
                 bundle_elements.erase(cut_it, end(bundle_elements));
+                cut_element_index = i;
                 // elements above cut point move to the next bundle
+                ++bundle_id;
                 for(auto const& element_index : bundle_elements) {
                     ret[element_index].bundle_id = bundle_id;
                 }
-                cut_element_index = i;
             }
         } else {
             // file is too big to be bundled
@@ -51,7 +58,6 @@ std::vector<FileBundleInfo> bundleFiles(std::vector<FileInfo> const& files, std:
             ret.push_back(bi);
         }
     }
-    // bundle all remains
     return ret;
 }
 
--- src/file_bundling.hpp
+++ src/file_bundling.hpp
@@ -7,6 +7,19 @@ struct FileBundleInfo {
     std::uint32_t bundle_id;
 };
 
+/** Groups elements from the file list into bundles of a minimum size.
+ * Grouping is done by assigning each element from the file list a bundle id.
+ * Elements that are by themselves already bigger than the minimum size remain unbundled and get a bundle id of 0.
+ * Smaller elements share the same bundle id such that the total size of all elements in a bundle exceeds
+ * the minimum size, but is never bigger than three times the minimum size.
+ * If the total size of all elements smaller than the minimum size does not exceed the minimum size, all of those
+ * elements get assigned to the same bundle.
+ * @param[in] files The list of files to be bundled.
+ * @param[in] min_unbundled_size The minimum size in bytes for a bundle of files.
+ * @return The list of bundle ids for the elements in files. Each element in this vector corresponds to the element
+ *         in files of the same index. Bundle ids are strictly increasing. After a bundle id of N in the vector, all
+ *         following ids will be >=N or 0.
+ */
 std::vector<FileBundleInfo> bundleFiles(std::vector<FileInfo> const& files, std::uintmax_t min_unbundled_size);
 
 #endif
