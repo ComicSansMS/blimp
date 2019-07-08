581dff276bfa5366cde4a78c14fb6fc17a811cf9
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-07-08 20:38:27 +0200

    added proper support for blimp buildbox

---
+++ cmake/FindBlimpBuildBox.cmake
@@ -0,0 +1,9 @@
+find_path(BLIMP_BUILDBOX_DIRECTORY
+    NAMES blimp_buildbox.marker
+    HINTS ${PROJECT_SOURCE_DIR}/../buildbox ${PROJECT_SOURCE_DIR}/../blimp-buildbox
+    PATH_SUFFIXES install
+    DOC "Blimp Buildbox Directory"
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(BlimpBuildbox DEFAULT_MSG BLIMP_BUILDBOX_DIRECTORY)
